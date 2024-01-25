6a0c93f2bb1194992b6d8f6fd47ee2b3aee4d86f
Anas0055 <manaskhan969@gmail.com>
2024-01-25 05:36:12 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates 
+// to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another
+//  5 tests evaluate to False.
+var num1 = 5;
+var num2 = 10;
+var str1 = "hello";
+var str2 = "world";
+var bool1 = true;
+var bool2 = false;
+// Test 1
+console.log("Is num1 equal to num2? I predict False.");
+console.log(num1 === num2);
+// Test 2
+console.log("Is num1 not equal to num2? I predict True.");
+console.log(num1 !== num2);
+// Test 3
+console.log("Is str1 equal to str2? I predict False.");
+console.log(str1 === str2);
+// Test 4
+console.log("Is str1 not equal to str2? I predict True.");
+console.log(str1 !== str2);
+// Test 5
+console.log("Is num1 less than num2? I predict True.");
+console.log(num1 < num2);
+// Test 6
+console.log("Is num1 greater than num2? I predict False.");
+console.log(num1 > num2);
+// Test 7
+console.log("Is bool1 equal to bool2? I predict False.");
+console.log(bool1 === true);
+// Test 8
+console.log("Is bool1 not equal to bool2? I predict True.");
+console.log(bool2 !== false);
+// Test 9
+console.log("Is bool1 true? I predict True.");
+console.log(bool1);
+// Test 10
+console.log("Is bool2 false? I predict True.");
+console.log(bool2);
---
+++ index.ts
@@ -0,0 +1,62 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates 
+// to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another
+//  5 tests evaluate to False.
+
+let num1: number = 5;
+let num2: number = 10;
+let str1: string = "hello";
+let str2: string = "world";
+let bool1= true;
+let bool2 = false;
+
+// Test 1
+console.log("Is num1 equal to num2? I predict False.");
+console.log(num1 === num2);
+
+// Test 2
+console.log("Is num1 not equal to num2? I predict True.");
+console.log(num1 !== num2);
+
+// Test 3
+console.log("Is str1 equal to str2? I predict False.");
+console.log(str1 === str2);
+
+// Test 4
+console.log("Is str1 not equal to str2? I predict True.");
+console.log(str1 !== str2);
+
+// Test 5
+console.log("Is num1 less than num2? I predict True.");
+console.log(num1 < num2);
+
+// Test 6
+console.log("Is num1 greater than num2? I predict False.");
+console.log(num1 > num2);
+
+// Test 7
+console.log("Is bool1 equal to bool2? I predict False.");
+console.log(bool1 === true);
+
+// Test 8
+console.log("Is bool1 not equal to bool2? I predict True.");
+console.log(bool2 !== false);
+
+// Test 9
+console.log("Is bool1 true? I predict True.");
+console.log(bool1);
+
+// Test 10
+console.log("Is bool2 false? I predict True.");
+console.log(bool2);
